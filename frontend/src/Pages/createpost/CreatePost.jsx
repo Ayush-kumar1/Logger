d4870eccf23e13217de7d1e3b4c54d424de81bdb
Ayush <ayush.ak5555@gmail.com>
2021-10-05 22:17:54 +0530

    refactor:Formatted code for create post page

--- frontend/src/Pages/createpost/CreatePost.jsx
+++ frontend/src/Pages/createpost/CreatePost.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import "./createpost.css";
 import { Input } from "@mui/material";
 import { Button } from "@mui/material";
 
 function CreatePost() {
-    return (
-        <div>
-             <div style={{ paddingTop: "9rem" }}>
+  return (
+    <div>
+      <div style={{ paddingTop: "9rem" }}>
         <div className="media_card-createpost">
           <h1 className="brand-logo" style={{ margin: "0 auto" }}>
             Logo
@@ -15,32 +15,21 @@ function CreatePost() {
           <div
             style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
           >
-            <Input
-              type="text"
-              placeholder="title"
-             
-              
-            />
-            <Input
-              type="text"
-              placeholder="body"
-              
-              
-            />
-            <Input type="text"  />
+            <Input type="text" placeholder="title" />
+            <Input type="text" placeholder="body" />
+            <Input type="text" />
             <Button
               variant="contained"
               color="secondary"
               style={{ width: "9rem", margin: "0px auto" }}
-              
             >
               Submit post
             </Button>
           </div>
         </div>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
